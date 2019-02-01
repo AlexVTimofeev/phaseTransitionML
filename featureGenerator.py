57a51441fa29c7fc4e0767952b2fdcfb4533fb9e
Evgeniy <evgen19-97@ya.ru>
2019-02-01 15:42:59 +0300

    add no global min

--- featureGenerator.py
+++ featureGenerator.py
@@ -13,7 +13,7 @@ startCount = 255
 
 
 def main():
-    rows = ['c{}'.format(i) for i in range(9)]
+    rows = ['c{}'.format(i) for i in range(9)] # 11, если добавлять положение равновесия
     df = pd.DataFrame(columns=rows)
     
     for logFile, rdfFile in zip(
@@ -26,14 +26,18 @@ def main():
         df_t = pd.read_csv(logFile,delimiter=' ',index_col='Step')
         df_rfd = pd.read_table(rdfFile,skiprows=[0,1,2,3,4], delimiter=' ',names=['Row','c_1[1]','c_1[2]','c_1[3]'],index_col='Row')
 
-        df_rfd = df_rfd.loc[df_rfd['c_1[2]'] != 0.000000]
+        # df_rfd = df_rfd.loc[df_rfd['c_1[2]'] != 0.000000]
         df_rfd = df_rfd['c_1[2]']
 
         data = [temp,press]
-        data += [df_rfd.max()/df_rfd.min()]
+        # data += [df_rfd.max()/df_rfd.min()]
+        data += [df_rfd.max()/df_rfd[df_rfd.argmax():].argmin()]
        
         for energy in ['PotEng','KinEng','TotEng']:
             data += [df_t[energy][startCount:].mean(),df_t[energy][startCount:].std()]
+
+        # data += [df_t['c_2[4]'].max(),df_t['c_3[4]'].max()] # add it
+
 
         df.loc[len(df)] = data
     
